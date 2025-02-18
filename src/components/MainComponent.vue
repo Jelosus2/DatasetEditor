62df5592bed12e72f37339e64872db617c025a9c
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2025-02-18 18:42:24 +0100

    started UI for dataset section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,75 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="tabs-lift tabs h-[85%]">
+  <div class="tabs-lift tabs h-[calc(100vh-86px)]">
     <input type="radio" name="dataset_tabs" class="tab" aria-label="Dataset" checked />
-    <div class="tab-content h-full border-base-300 bg-base-100 p-3">Content</div>
+    <div class="tab-content !flex justify-between overflow-auto border-base-300 bg-base-100 p-3">
+      <div>a</div>
+      <div class="divider m-1 divider-horizontal"></div>
+      <div>b</div>
+      <div class="divider m-1 divider-horizontal"></div>
+      <div class="flex">
+        <div class="flex flex-col">
+          <div>c</div>
+          <div>d</div>
+        </div>
+        <ul class="menu rounded-box bg-base-200">
+          <li>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
