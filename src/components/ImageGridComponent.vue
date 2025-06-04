b1cc279c12e3482b9c7898874d189f38c6d3dd5a
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2025-06-04 20:44:45 +0200

    Fixed image grid flickering

--- src/components/ImageGridComponent.vue
+++ src/components/ImageGridComponent.vue
@@ -29,7 +29,7 @@ const imageEntries = computed(() => {
 
 <template>
   <div
-    class="grid h-fit grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] gap-1 overflow-auto scroll-smooth"
+    class="grid h-fit grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] gap-1 overflow-y-auto overflow-x-hidden scroll-smooth scrollbar-stable"
   >
     <VirtualImage
       v-for="[name, image] in imageEntries"
